19a31667eab4dc804d5cff91496aac31b0a6993e
Anand Pradeep <anandpradeep181@gmail.com>
2025-08-05 15:44:19 +0530

    last one

--- DSA_1/search.js
+++ DSA_1/search.js
@@ -40,3 +40,21 @@ console.log(search([-1, 0, 3, 5, 9, 12], 9));
 console.log(search([-1, 0, 3, 5, 9, 12], 69));
 
 
+    //-binary-search.js  recursive implementation 
+ 
+
+
+function BinarySearch(nums,target,start=0,end=nums.length-1){
+    if(start>end){
+        return -1
+    }
+    let middle=Math.floor((start+end)/2)
+    if(nums[middle]===target){
+        return middle
+    }else if(nums[middle]<target){
+        return BinarySearch(nums,target,middle+1,end)
+    }else{
+        return BinarySearch(nums,target,start,middle-1)
+    }
+}
+
